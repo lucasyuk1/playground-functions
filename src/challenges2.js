4df107599fbd86b8b007154e435679e571819b09
LucasCS <lcsvdk@gmail.com>
2022-09-27 18:06:16 -0300

    Resolvendo requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,19 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+  
+  return number.replace(/\D/g,"").replace(/^(\d{2})(\d)/g,"($1) $2").replace(/(\d)(\d{4})$/,"$1-$2"); 
+
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > (lineB+lineC) || lineB > (lineA+lineC) || lineC > (lineB+lineA)){
+    return false;
+  }
+  if (lineA < (lineB+lineC) && lineA > Math.abs(lineB-lineC) || lineB < (lineA+lineC) && lineB > Math.abs(lineA-lineC) || lineC < (lineB+lineA) && lineC > Math.abs(lineB-lineA)){
+    return true;
+
+  }
 }
 
 // Desafio 13
